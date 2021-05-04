6cf8d5bb77bc7025c3258e9ed1ce5af029e24818
tangpeng1 <tangpeng1@vipkid.com.cn>
2021-05-05 01:20:41 +0800

    add TimeDomainOfA

--- matlab-web/src/test/java/com/tp/math/matlab/timedomain/acceleration/TimeDomainOfATest.java
+++ matlab-web/src/test/java/com/tp/math/matlab/timedomain/acceleration/TimeDomainOfATest.java
@@ -1,11 +1,7 @@
 package com.tp.math.matlab.timedomain.acceleration;
 
-import com.tp.math.matlab.extension.acceleration.core.*;
-import com.tp.math.matlab.extension.acceleration.core.Filt.FiltResult;
-import com.tp.math.matlab.extension.acceleration.core.ValueOfPeak.ValueOfPeakResult;
-import com.tp.math.matlab.kernel.core.DoubleMax;
+import com.tp.math.matlab.extension.acceleration.core.TimeDomainOfA;
 import com.tp.math.matlab.kernel.util.ExcelUtils;
-import com.tp.math.matlab.kernel.util.PythonUtils;
 import com.tp.math.matlab.service.FirFilterService;
 import com.tp.math.matlab.service.FirWindowService;
 import lombok.extern.slf4j.Slf4j;
@@ -43,37 +39,7 @@ public class TimeDomainOfATest {
         //a=xlsRead('1414.xlsx',2);
         //inputArray=a(:,8);
         final List<Double> a = ExcelUtils.xlsRead(fileName, TEST_EXCEL_COLUMN_INDEX - 1);
-        final long fs = 25600;           //%采样频率
-        //n=length(inputArray);
-        final int N = a.size();   //%数据长度
-        //df=fs/N;
-        final double df = fs / N;
-        final double fcut = 5;          //%低频截止
-        final double fmin = 5;          //%fmin：起始频率
-        final double fmax = 10000;      //famx：终止频率
-        final double time = (double) N / fs;
-        //[a_fir,mf]=filt(a,fs,fcut,fs/2.25);
-        final FiltResult filtResult = new Filt(a, fs, fcut, fs / 2.25).getResult();
-        final double RPM = filtResult.get_mf() * 60;
-        //[p,m]=max(a_fir);
-        final DoubleMax pm_max = PythonUtils.getMax(filtResult.get_Afir());
-        //tm=m/fs;
-        final double tm = pm_max.getIndex() / fs;
-        final double A = pm_max.getVal();
-        //[Pp,Np]=Value_of_Peak(a_fir);
-        final ValueOfPeakResult valueOfPeakResult = new ValueOfPeak(filtResult.get_Afir()).getResult();
-        //[vmean]=Mean_Value(a_fir);
-        final double vmean = new MeanValue(filtResult.get_Afir()).getResult();
-        //[sigma]=Value_of_Sigma(a_fir,vmean);
-        final double sigma = new ValueOfSigma(filtResult.get_Afir(), vmean).getResult();
-        //[vrms]=Value_of_RMS(a_fir);
-        final double vrms = new ValueOfRMS(filtResult.get_Afir()).getResult();
-        //pf=p/vrms;
-        final double pf = pm_max.getVal() / vrms;
-        //[ske]=Value_of_Skewness(a_fir,vmean);
-        final double ske = new ValueOfSkeness(filtResult.get_Afir(), vmean).getResult();
-        //[ske]=Value_of_Kurtosis(a_fir,vmean,sigma);
-        final double kur = new ValueOfKurtosis(filtResult.get_Afir(), vmean, sigma).getResult();
-        //[TV]=total_value(a,fs,5,10000,16);
+        final TimeDomainOfA main = new TimeDomainOfA();
+        main.run(a);
     }
 }
