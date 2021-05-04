2808737e4b09f1d5bb6e9b4d8a3612a0c7f0eefd
tangpeng1 <tangpeng1@vipkid.com.cn>
2021-05-04 22:28:39 +0800

    add ValueOfPeak

---
+++ matlab-kernel/src/main/java/com/tp/math/matlab/kernel/timedomain/acceleration/ValueOfPeak.java
@@ -0,0 +1,58 @@
+package com.tp.math.matlab.kernel.timedomain.acceleration;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.Accessors;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by tangpeng on 2021-05-04
+ */
+@Slf4j
+@Accessors(chain = true)
+public class ValueOfPeak {
+
+    /**
+     * 源数据
+     */
+    private List<Double> inputArray;
+    /**
+     * result
+     */
+    @Getter
+    private ValueOfPeakResult result;
+
+    public ValueOfPeak(@NonNull final List<Double> inputArray) {
+        this.inputArray = inputArray;
+        this.result = transform();
+    }
+
+    private ValueOfPeakResult transform() {
+        final int n = this.inputArray.size();
+        final double[] x = new double[n];
+        final double[] y = new double[n];
+        for (int i = 0; i < this.inputArray.size(); i++) {
+            final double cur = this.inputArray.get(i);
+            if (cur > 0) {
+                x[i] = cur;
+            } else {
+                y[i] = cur;
+            }
+        }
+        final double _Pp = Arrays.stream(x).max().getAsDouble();
+        final double _Np = Arrays.stream(y).min().getAsDouble();
+        return ValueOfPeakResult.of(_Pp, _Np);
+    }
+
+
+    @Getter
+    @RequiredArgsConstructor(staticName = "of")
+    public static class ValueOfPeakResult {
+        private final double _Pp;
+        private final double _Np;
+    }
+}
--- matlab/src/test/java/com/tp/math/matlab/timedomain/acceleration/TimeDomainOfATest.java
+++ matlab/src/test/java/com/tp/math/matlab/timedomain/acceleration/TimeDomainOfATest.java
@@ -2,6 +2,9 @@ package com.tp.math.matlab.timedomain.acceleration;
 
 import com.tp.math.matlab.kernel.core.DoubleMax;
 import com.tp.math.matlab.kernel.timedomain.acceleration.Filt;
+import com.tp.math.matlab.kernel.timedomain.acceleration.Filt.FiltResult;
+import com.tp.math.matlab.kernel.timedomain.acceleration.ValueOfPeak;
+import com.tp.math.matlab.kernel.timedomain.acceleration.ValueOfPeak.ValueOfPeakResult;
 import com.tp.math.matlab.kernel.transform.FirFilter;
 import com.tp.math.matlab.kernel.transform.FirWindow;
 import com.tp.math.matlab.kernel.util.ExcelUtils;
@@ -55,46 +58,15 @@ public class TimeDomainOfATest {
         final double fmax = 10000;      //famx：终止频率
         final double time = (double) N / fs;
         //[a_fir,mf]=filt(a,fs,fcut,fs/2.25);
-        final Filt.FiltResult filtResult = new Filt(records, fs, fcut, fs / 2.25).getResult();
+        final FiltResult filtResult = new Filt(records, fs, fcut, fs / 2.25).getResult();
         final double RPM = filtResult.getMf() * 60;
         //[p,m]=max(a_fir);
         final DoubleMax pm_max = PythonUtils.getMax(filtResult.getA_fir());
         //tm=m/fs;
         final double tm = pm_max.getIndex() / fs;
         final double A = pm_max.getVal();
+        //[Pp,Np]=Value_of_Peak(a_fir);
+        final ValueOfPeakResult valueOfPeakResult = new ValueOfPeak(filtResult.getA_fir()).getResult();
 
-//        //t=(0:n-1)/fs
-//        final List<Double> t = IntStream.range(0, n)
-//                .mapToDouble(i -> (double) i)
-//                .map(i -> i / fs)
-//                .boxed()
-//                .collect(toList());
-//        log.info("t=(0:n-1)/fs result:\n" + t);
-//        //f=(0:n-1)/fs
-//        final List<Double> f = t;
-//        //N=100
-//        final int N = 100;
-        //window=hann(N+1)
-//        final HannWindow window = hannWindowService.transform(N + 1);
-//        log.info("window=hann(N+1) result:\n" + window.getResult());
-        //wp1=[5/fs*2 10000/fs*2]
-        final List<Double> wp1 = DoubleStream.of(5, 10000)
-                .boxed()
-                .map((i -> i / fs * 2))
-                .collect(toList());
-        log.info("wp1=[5/fs*2 10000/fs*2] result:\n" + wp1);
-        // fir=fir1(N,wp1,window);
-        final FirWindow d = firWindowService.fir1(N + 1, wp1);
-        log.info("fir=fir1(N,wp1,window) result:\n" + d.format().toString());
-        // x1 = filter(fir,1,inputArray);
-        final FirFilter firFilter = firFilterService.filter(d.getResult(), records);
-        log.info(firFilter.getResult().toString());
-
-        //matlab output
-//        final URL resource = this.getClass().getResource(String.format(FILTER_RESULT_TXT, HANNING_LENGTH, FIR1_CUT_OFF, FIR1_NUM_TAPS));
-//        final List<String> expecteds = Files.lines(Paths.get(resource.toURI()))
-//                .map(String::trim)
-//                .collect(toList());
-//        AssertUtils.stringEquals(expecteds, firFilter.format());
     }
 }
