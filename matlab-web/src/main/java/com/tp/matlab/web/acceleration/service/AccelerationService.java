1f79486579008a424ff46eac22242de0695ed1b4
tangpeng1 <tangpeng1@vipkid.com.cn>
2021-05-06 14:19:03 +0800

    rename package

---
+++ matlab-web/src/main/java/com/tp/matlab/web/acceleration/service/AccelerationService.java
@@ -0,0 +1,19 @@
+package com.tp.matlab.web.acceleration.service;
+
+import com.tp.matlab.extension.acceleration.core.TimeDomainOfA;
+import lombok.NonNull;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by tangpeng on 2021-05-06
+ */
+@Service
+public class AccelerationService {
+
+    public void run(@NonNull final List<Double> a) {
+        new TimeDomainOfA().run(a);
+    }
+
+}
