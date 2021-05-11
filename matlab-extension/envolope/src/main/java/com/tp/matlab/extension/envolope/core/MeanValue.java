e6707d57d77be9f6626b266b653e46a9b60b63a0
tangpeng1 <tangpeng1@vipkid.com.cn>
2021-05-12 00:18:09 +0800

    init envolope

---
+++ matlab-extension/envolope/src/main/java/com/tp/matlab/extension/envolope/core/MeanValue.java
@@ -0,0 +1,26 @@
+package com.tp.matlab.extension.envolope.core;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by tangpeng on 2021-05-04
+ */
+@RequiredArgsConstructor
+class MeanValue {
+
+    /**
+     * 源数据
+     */
+    private final List<Double> a;
+
+    public double execute() {
+        final int n = this.a.size();
+        double sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += this.a.get(i);
+        }
+        return sum / n;
+    }
+}
